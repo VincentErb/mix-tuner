192f020a988997d813e3060624b6768c87d76806
Vincent Erb <vincent.erb@hotmail.fr>
2026-04-26 18:55:12 -0400

    feat: for screen to stay on while tuner is active

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:wakelock_plus/wakelock_plus.dart';
 import 'app.dart';
 import 'providers/persistence_provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  WakelockPlus.enable();
   final prefs = await SharedPreferences.getInstance();
   runApp(
     ProviderScope(
